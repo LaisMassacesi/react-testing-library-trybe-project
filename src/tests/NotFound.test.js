40bd7422cf908ed435ee4ecadc4809271969c6f0
Lais <laismassacesi@gmail.com>
2022-07-19 17:49:02 -0300

    Requisito 3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Verifique o componente Not Found', () => {
+  test('Teste se a página contém um heading com o devido texto', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTxt = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+    expect(notFoundTxt).toBeInTheDocument();
+  });
+
+  test('Verifique se a página mostra a devida imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/Pikachu crying/i);
+    const srcImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(img.src).toContain(`${srcImg}`);
+  });
+});
