26488cd45f738fad41d0c4cc56e545f6b4efa2f1
Lais <laismassacesi@gmail.com>
2022-07-19 16:54:44 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWhithRouter from '../renderWithRouter';
+
+describe('Teste se a página About contém as informações sobre a Pokédex', () => {
+  test('Teste se a página contém um heading com o texto About Pokédex;', () => {
+    renderWhithRouter(<About />);
+
+    const aboutTxt = screen.getByRole('heading',
+      { name: /About Pokédex/i }, { level: 2 });
+    expect(aboutTxt).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWhithRouter(<About />);
+
+    const describeTxt = screen.getByText(
+      /This application simulates a Pokédex, a digital encyclopedia/i,
+    );
+    expect(describeTxt).toBeInTheDocument();
+
+    const detailsTxt = screen.getByText(
+      /One can filter Pokémons by type, and see more details /i,
+    );
+    expect(detailsTxt).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWhithRouter(<About />);
+
+    const verifyImg = screen.getAllByAltText(/Pokédex/i);
+    expect(verifyImg).toBeDefined();
+  });
+});
